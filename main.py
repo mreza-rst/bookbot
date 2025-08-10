fa41af630815887dddea70720fc6168636c5748d
mrezarst <mreza.rostamiani@gmail.com>
2025-08-10 12:45:19 +0330

    finished the output but i think \n shouldn't actually go to the next line in the output

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ def file_to_str(filepath):
         return b.read()
 
 def main():
-    frankenstein = file_to_str("books/frankenstein.txt")
-    num_words = word_count(frankenstein)
-    num_chars = char_count(frankenstein)
+    path = "books/frankenstein.txt"
+    book = file_to_str(path)
+    num_words = word_count(book)
+    num_chars = char_count(book)
     sorted_chars = sort_chars(num_chars)
-    print(f"{num_words} words found in the document")
-    print(sorted_chars)
 
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for c in sorted_chars:
+        print(f"{c["char"]}: {c["num"]}")
+    print("============= END ===============")
 main()
