4a36017cbdf79c002e7d79e03de904815588dc35
mrezarst <mreza.rostamiani@gmail.com>
2025-08-11 10:01:50 +0330

    add argument

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from stats import sort_chars, word_count, char_count
+import sys
 
 def file_to_str(filepath):
     with open(filepath) as b:
         return b.read()
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
     book = file_to_str(path)
     num_words = word_count(book)
     num_chars = char_count(book)
